9fe95aa8dc2dc0b7115abf49953cd0a612ebd4b9
Harshad Ahammed <harshadaju@gmail.com>
2021-03-29 23:26:53 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Component1 from "./component";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Component1 />
+    </div>
+  );
+}
---
+++ src/component.tsx
@@ -0,0 +1,6 @@
+const Component1 = () => (
+  <div id="container1">
+    <h1>Hai</h1>
+  </div>
+);
+export default Component1;
